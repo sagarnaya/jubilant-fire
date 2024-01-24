9ebd6f6bc2b681978eeaaa3b9ae617f14c645b08
sagarnaya <130038697+sagarnaya@users.noreply.github.com>
2024-01-24 22:22:21 +0530

    Update and rename 5_6066697810189749058.sql to SQLfile.sql

---
+++ SQLfile.sql
@@ -0,0 +1,233 @@
+﻿-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: bank-mgmt-sys
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `accounttype` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `balance` double NOT NULL,
+  `status` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `email` (`email`),
+  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`email`) REFERENCES `user` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (21,'regular','admin@gmail.com',45,'approved'),(22,'regular','admin@gmail.com',100,'declined'),(23,'savings','user1@gmail.com',107.5,'approved'),(24,'savings','admin@gmail.com',30.5,''),(25,'credit','admin@gmail.com',25.456,'declined'),(26,'credit','admin@gmail.com',3,'pending'),(27,'debit','sagar@gmail.com',60,'approved'),(28,'savings','sagar@gmail.com',12,'approved');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bank_transaction`
+--
+
+DROP TABLE IF EXISTS `bank_transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bank_transaction` (
+  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender_account` int(11) NOT NULL,
+  `receiver_account` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `amount` double NOT NULL,
+  `transaction_date` date NOT NULL,
+  PRIMARY KEY (`transaction_id`),
+  KEY `sender_account` (`sender_account`),
+  KEY `receiver_account` (`receiver_account`),
+  CONSTRAINT `bank_transaction_ibfk_1` FOREIGN KEY (`sender_account`) REFERENCES `account` (`id`),
+  CONSTRAINT `bank_transaction_ibfk_2` FOREIGN KEY (`receiver_account`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bank_transaction`
+--
+
+LOCK TABLES `bank_transaction` WRITE;
+/*!40000 ALTER TABLE `bank_transaction` DISABLE KEYS */;
+INSERT INTO `bank_transaction` VALUES (1,23,24,'desc ript',15,'2023-03-02'),(2,23,24,'desc ript',12,'2023-03-02'),(3,24,23,'this is my first transaction',156,'2023-01-01'),(4,21,23,'hoof',50,'2024-01-01'),(5,21,23,'some',5,'2023-01-01'),(6,27,23,'2dn testing transaction',1000,'2024-04-06'),(7,27,23,'23232',40,'2024-04-06');
+/*!40000 ALTER TABLE `bank_transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `beneficiary`
+--
+
+DROP TABLE IF EXISTS `beneficiary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `beneficiary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `beneficiary_name` varchar(255) NOT NULL,
+  `beneficiary_email` varchar(255) NOT NULL,
+  `beneficiary_account` int(11) NOT NULL,
+  `sender_account` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `beneficiary_email` (`beneficiary_email`),
+  KEY `beneficiary_account` (`beneficiary_account`),
+  CONSTRAINT `beneficiary_ibfk_1` FOREIGN KEY (`beneficiary_email`) REFERENCES `user` (`email`),
+  CONSTRAINT `beneficiary_ibfk_2` FOREIGN KEY (`beneficiary_account`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `beneficiary`
+--
+
+LOCK TABLES `beneficiary` WRITE;
+/*!40000 ALTER TABLE `beneficiary` DISABLE KEYS */;
+INSERT INTO `beneficiary` VALUES (1,'b-n','admin@gmail.com',23,24),(2,'myfriend','user1@gmail.com',23,24),(3,'user1@gmail.com','user1@gmail.com',23,24),(4,'user1@gmail.com','user1@gmail.com',23,24),(5,'my best buddy','admin@gmail.com',25,23),(6,'user 1 ben','user1@gmail.com',23,21),(7,'abeneficiary name','user1@gmail.com',23,27);
+/*!40000 ALTER TABLE `beneficiary` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `card`
+--
+
+DROP TABLE IF EXISTS `card`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `card` (
+  `card_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `card_type` varchar(255) NOT NULL,
+  PRIMARY KEY (`card_id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `card_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `card`
+--
+
+LOCK TABLES `card` WRITE;
+/*!40000 ALTER TABLE `card` DISABLE KEYS */;
+INSERT INTO `card` VALUES (1,23,'debit'),(2,24,'debit'),(3,24,'debit'),(4,24,'credit'),(5,23,'credit'),(6,27,'credit');
+/*!40000 ALTER TABLE `card` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loan`
+--
+
+DROP TABLE IF EXISTS `loan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `loan` (
+  `loan_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `loan_type` varchar(255) NOT NULL,
+  `loan_amount` double NOT NULL,
+  `loan_interest` double NOT NULL,
+  `loan_completion_date` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  PRIMARY KEY (`loan_id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `loan_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loan`
+--
+
+LOCK TABLES `loan` WRITE;
+/*!40000 ALTER TABLE `loan` DISABLE KEYS */;
+INSERT INTO `loan` VALUES (1,24,'homeloan',100.5,9.5,'2023-03-02','approved'),(2,21,'homeloan',100.5,9.5,'2023-03-02',''),(3,23,'homeloan',100.5,7.5,'2023-03-02',''),(4,24,'personalloan',2.3,11.45,'2024-01-01',''),(5,27,'homeloan',30,2,'2024-05-07','approved');
+/*!40000 ALTER TABLE `loan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `passbook`
+--
+
+DROP TABLE IF EXISTS `passbook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `passbook` (
+  `passbook_id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `issued_date` date NOT NULL,
+  PRIMARY KEY (`passbook_id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `passbook_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `passbook`
+--
+
+LOCK TABLES `passbook` WRITE;
+/*!40000 ALTER TABLE `passbook` DISABLE KEYS */;
+INSERT INTO `passbook` VALUES (1,24,'2023-03-02'),(2,24,'2023-03-02'),(3,23,'2023-03-04'),(4,24,'2023-03-04'),(5,27,'2024-04-06');
+/*!40000 ALTER TABLE `passbook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `person_index_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (4,'admin@gmail.com','admin','manager','admin','ln'),(6,'user1@gmail.com','u1','customer','user','1'),(7,'some1@gmail.com','some1','customer','some','person'),(9,'sagar@gmail.com','abcd','customer','sagar','abcd');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-06  0:51:00
